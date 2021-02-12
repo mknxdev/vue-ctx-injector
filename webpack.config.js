6caa2939a28b9d8494c8ffaeb97124b3f138468e
paulo <paul.guzdariviere@ecoco2.com>
2021-02-12 15:10:26 +0100

    Add necessary pkg & config for library export

--- webpack.config.js
+++ webpack.config.js
@@ -7,21 +7,15 @@ const TerserPlugin = require('terser-webpack-plugin')
 module.exports = {
   entry: './index.js',
   output: {
+    library: 'VueCtxInjector',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
     path: path.resolve(__dirname, './dist/'),
     publicPath: path.resolve(__dirname, './dist/'),
     filename: 'vue-ctx-injector.js',
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src/static/js/'),
-    }
-  },
   module: {
     rules: [
-      // JS compilation
-      //
-      // Note: Target browsers list is available in the .browserslistrc file.
-      // It is set to include all browsers supporting ES5.
       {
         test: /\.js$/,
         use: [
@@ -39,6 +33,6 @@ module.exports = {
   optimization: {
     minimizer: [
       new TerserPlugin(),
-    ]
+    ],
   },
 }
