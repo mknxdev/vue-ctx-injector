0f45aa7b6341b43bd692e7b0f359135c1066856b
paulo <paul.guzdariviere@ecoco2.com>
2021-02-25 22:46:16 +0100

    Add coverage report for unit tests

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,21 @@ module.exports = {
     '@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
-    "\\.js$": "babel-jest",
+    '\\.js$': ['babel-jest', {
+      plugins: ['@babel/plugin-proposal-class-properties'],
+    }],
   },
+
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{js,jsx}'
+  ],
+  coverageDirectory: '<rootDir>/tests/unit/_coverage',
+  // coverageThreshold: {
+  //   global: {
+  //     statements: 62,
+  //     branches: 53,
+  //     functions: 67,
+  //     lines: 63
+  //   }
+  // },
 }
